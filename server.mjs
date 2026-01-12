69bb128d54678339d6eb934e912a9e8c694ae9e3
Marius <mariuswha@outlook.com>
2026-01-12 15:48:34 +0100

    First scaffold and server

---
+++ server.mjs
@@ -0,0 +1,14 @@
+import express from "express";
+
+//const PORT = system.env.PORT || 8080;
+const PORT = 8080;
+const app = new express();
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
+
+app.listen(PORT, () => {
+  console.log('Server is running ')
+})
