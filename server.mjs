81d1d9757322b9ab9ca7aecdbe7074adc04bca9a
Marius <mariuswha@outlook.com>
2026-01-21 15:16:24 +0100

    Router and App in one file

--- server.mjs
+++ server.mjs
@@ -4,10 +4,21 @@ import express from "express";
 const PORT = 8080;
 const app = new express();
 
+const settingsRouter = express.Router();
+
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.send('Hello World')
+  res.send('Hello Worlds');
 })
+
+
+app.get('/settings', (req, res) => {
+  res.send("settings");
+})
+
+app.use('/settings', settingsRouter);
+
 
 app.listen(PORT, () => {
   console.log('Server is running')
