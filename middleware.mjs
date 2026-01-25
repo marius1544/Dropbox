d5928c88b6c6f6250ba92622b0b86ecaaaf0285f
Marius <mariuswha@outlook.com>
2026-01-25 19:07:19 +0100

    Changed validateFileType to be a middleware & Cleaned up

--- middleware.mjs
+++ middleware.mjs
@@ -3,19 +3,18 @@ for (let i = 0; i < fileEndings.length; i++) {
     const file = fileEndings[i];
 }
 
+export function validateFileType(req, res, next){
+    const fileType = req.body.filename
 
-
-let aFileTest = 'mytest.exe'
-
-export function validateFileType(fileType){
     const fileEnding = fileType.split('.').pop();
      const isValid = fileEndings.find(item => item === fileEnding);
 
    if(isValid){
     console.log(fileType + " is a valid file type");
+    next();
    } else {
     console.log(fileType + " is not a valid file type");
+    res.status(400).send("Invalid file type");
    }
 }
 
-validateFileType(aFileTest)
