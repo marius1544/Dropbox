3aa73c6b8a7288fc35d22d42b3a667d4189258f1
Marius <mariuswha@outlook.com>
2026-01-27 16:04:08 +0100

    Added a username checker which is going to take input from a user input field

--- routes/userAPI.mjs
+++ routes/userAPI.mjs
@@ -1,14 +1,22 @@
 import express from "express";
 import { generateID } from "../dataObjects/user.mjs"
 import user from "../dataObjects/user.mjs"
-import settingsRouter from "./settingsAPI.mjs";
+function generateUsername(userName){
+  return userName;
+}
 const userRouter = express.Router();
 userRouter.use(express.json())
 
 userRouter.post('/', (req, res) => {
   let newUser = user();
   newUser.id = generateID(); 
-
+  newUser.username = "";
+  
+ if(newUser.username === ""){
+  newUser.username = generateUsername("Mariusr");
+} else {
+  console.log("After if", newUser.username = "");
+}
   res.json(JSON.stringify(newUser));
 })
 
