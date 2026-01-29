258cfff631b1896d1265d00d83e1085c00a0fe48
Marius <mariuswha@outlook.com>
2026-01-29 16:02:00 +0100

    Added retractBtn for account deletion

--- public/index.mjs
+++ public/index.mjs
@@ -1,6 +1,9 @@
 const TOSmenuCheckboxVar = document.getElementById("TOSmenuCheckbox");
 const submitTosBtn = document.getElementById("submitTOS");
 const userNameInp = document.getElementById("usernameInp");
+const formOutPutFieldVar = document.getElementById("formOutPutField");
+const retractBtn = document.getElementById("retract-delete");
+const userIdInp = document.getElementById("userIdInp");
 
 submitTosBtn.addEventListener("click", async (event) => {
   event.preventDefault();
@@ -26,7 +29,28 @@ submitTosBtn.addEventListener("click", async (event) => {
     });
     const data = await response.json();
     console.log(data);
+    formOutPutFieldVar.innerHTML = `User ${data}`;
   } catch {
     console.log("Feil ved opprettelse av bruker:");
   }
 });
+
+retractBtn.addEventListener("click", async () => {
+    const userimpVar = userIdInp.value;
+
+    try {
+      const response = await fetch(`/user/${userimpVar}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        id: userimpVar
+      }),
+    }); 
+    const data = await response.json();
+    console.log(data)
+    } catch{
+       console.log("Feil ved sletting av bruker:");
+    }
+})
